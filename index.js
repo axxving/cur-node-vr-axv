1517dddf42d096c075344c2498db481a2f598575
Irving Alejandro Hernández Ramírez <alex.ing.hz@gmail.com>
2024-09-17 21:13:58 -0600

    feat: create a node server
    Co-Authored-By: Irving Alejandro Hernández Ramírez <160192346+axxving@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
 const { conexion } = require("./baseDatos/coneccion");
+const express = require("express");
+const cors = require("cors");
 
 // Inicializar app
 console.log("App de node arrancada");
 
 // Conectar a la base de datos
 conexion();
+
+// Crear servidor de node js
+const app = express();
+const puerto = 3900;
+
+// Configurar el cors
+app.use(cors());
+
+// Convertir body a objeto js
+app.use(express.json());
+
+// Crear rutas
+
+// Crear el servidor y escuchar peticiones\
+app.listen(puerto, () => {
+  console.log("Servidor corriendo en el puerto " + puerto);
+});
