ca544f6a956d3dc7c9fcafc6feeddcbfc69efcfe
axxving <axxving@gmail.com>
2024-10-12 21:42:18 -0600

    feat: configuring node server
    Co-Authored-By: axxving <160192346+axxving@users.noreply.github.com>

--- api-red-social-02/index.js
+++ api-red-social-02/index.js
@@ -1,5 +1,7 @@
 // Importar dependencias
 const { conection } = require("./database/conection");
+const express = require("express");
+const cors = require("cors");
 
 // Mensaje de bienvenida
 console.log("API NODE para RED SOCIAL arrancada");
@@ -8,3 +10,26 @@ console.log("API NODE para RED SOCIAL arrancada");
 conection();
 
 // Crear un servidor de node
+const app = express();
+const puerto = 3900;
+
+// Configurar cors
+app.use(cors());
+
+// Convertir los datos del body a un json
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Cargar las rutas
+// Ruta de prueba
+app.get("/ruta-prueba", (req, res) => {
+  return res.status(200).json({
+    mensaje: "Hola mundo desde la ruta de prueba",
+    estado: true,
+  });
+});
+
+// Poner servidor a escuchar peticiones http
+app.listen(puerto, () => {
+  console.log(`Servidor escuchando en el puerto ${puerto}`);
+});
